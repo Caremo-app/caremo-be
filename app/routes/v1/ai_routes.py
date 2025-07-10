903d5f6c43c37204af9edca61fee5db7eb78b5f7
WazeAzure <bryan.rich0604@gmail.com>
2025-07-10 17:30:19 +0700

    feat: temp fix for template meta

--- app/routes/v1/ai_routes.py
+++ app/routes/v1/ai_routes.py
@@ -79,15 +79,28 @@ def predict(
 def simulate_family(name_persona: str, location: Location, payload: dict = Depends(verify_token), db: Session = Depends(get_db)):
     resolved_address = geo_service.reverse_geocode(location.latitude, location.longitude)
     
-    user_email = payload['sub']
-    
-    whatsapp_api.send_template_message(PhoneEnum.NO_AKMAL, 'health_warning', name_persona, 'SYSTEM', 150, resolved_address)
+    user_name = "AKMAL"
+    family_member_name = name_persona
+    abnormality_type = "Asystole"
+    measured_value = 150
+    msg = f"Hi {user_name}, this is a health update from Caremo.\n📌 We've detected an unusual pattern in {family_member_name}'s vital signs at {datetime.datetime.now().strftime("%d %B %Y, %H.%M WIB")}.\n🩺 Detected Issue: {abnormality_type}\n❤️ Heart Rate (BPM): {measured_value}\n📍 Location: {resolved_address}\n\nPlease check on them as soon as possible, or consult a healthcare professional if needed."
+    whatsapp_api.send_text_message(PhoneEnum.NO_AKMAL, msg, 'en')
+            
+    # whatsapp_api.send_template_message(PhoneEnum.NO_AKMAL, 'health_warning', name_persona, 'SYSTEM', 150, resolved_address)
     return {"msg": "Succeed"}
 
 @router.post("/simulate-hospital")
 def simulate_hospital(name_persona: str, location: Location, payload: dict = Depends(verify_token), db: Session = Depends(get_db)):
     resolved_address = geo_service.reverse_geocode(location.latitude, location.longitude)
-    whatsapp_api.send_template_message(PhoneEnum.NO_AKMAL, 'health_warning', name_persona, 'SYSTEM', 150, resolved_address)
+    
+    user_name = "AKMAL"
+    family_member_name = name_persona
+    abnormality_type = "Asystole"
+    measured_value = 150
+    msg = f"Hi {user_name}, this is a health update from Caremo.\n📌 We've detected an unusual pattern in {family_member_name}'s vital signs at {datetime.datetime.now().strftime("%d %B %Y, %H.%M WIB")}.\n🩺 Detected Issue: {abnormality_type}\n❤️ Heart Rate (BPM): {measured_value}\n📍 Location: {resolved_address}\n\nWe have call an ambulance to {resolved_address} for {name_persona}"
+        
+        
+    whatsapp_api.send_text_message(PhoneEnum.NO_AKMAL, msg, 'en')
     return {"msg": "Succeed"}
 
 @router.get("/twiml")
